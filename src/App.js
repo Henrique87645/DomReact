362237e5aad41a17fe7376b21cabcfde8bb4d957
Henrique87645 <henriquedominguez12@gmail.com>
2024-04-10 12:39:26 -0300

    10/04

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
   <>
-  <Header/>
-   <h1>testes</h1>
-   <p>testando o react</p>
-   <Footer/>
+    <Header/>
+  <Banner image="favoritos" />
+    <Container>
+      <h1>testes</h1>
+      <p>testando o react</p>
+    </Container>
+    <Footer/>
   </>
   );
 }
